c9f4878b69616be977d9ea521eda02a75f98aad0
Owen Harrison <owenbharrison@gmail.com>
2025-08-12 17:39:14 -0600

    logical device & queues

--- src/main.cpp
+++ src/main.cpp
@@ -65,6 +65,10 @@ private:
 
 	VkPhysicalDevice physical_device=VK_NULL_HANDLE;
 
+	VkDevice device;
+
+	VkQueue graphics_queue;
+
 	std::vector<const char*> getRequiredExtensions() {
 		uint32_t glfw_ext_ct=0;
 		const char** glfw_extensions;
@@ -163,21 +167,19 @@ private:
 	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
 		QueueFamilyIndices indices;
 
-		uint32_t queueFamilyCount=0;
-		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+		uint32_t queue_family_ct=0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_ct, nullptr);
 
-		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
-		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+		std::vector<VkQueueFamilyProperties> queue_families(queue_family_ct);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_ct, queue_families.data());
 
 		int i=0;
-		for(const auto& queueFamily:queueFamilies) {
-			if(queueFamily.queueFlags&VK_QUEUE_GRAPHICS_BIT) {
+		for(const auto& queue_family:queue_families) {
+			if(queue_family.queueFlags&VK_QUEUE_GRAPHICS_BIT) {
 				indices.graphics_family=i;
 			}
 
-			if(indices.isComplete()) {
-				break;
-			}
+			if(indices.isComplete()) break;
 
 			i++;
 		}
@@ -213,10 +215,48 @@ private:
 		}
 	}
 
+	void createLogicalDevice() {
+		QueueFamilyIndices indices=findQueueFamilies(physical_device);
+
+		VkDeviceQueueCreateInfo queue_create_info{};
+		queue_create_info.sType=VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queue_create_info.queueFamilyIndex=indices.graphics_family.value();
+		queue_create_info.queueCount=1;
+
+		float queue_priority=1.f;
+		queue_create_info.pQueuePriorities=&queue_priority;
+
+		VkPhysicalDeviceFeatures device_features{};
+
+		VkDeviceCreateInfo create_info{};
+		create_info.sType=VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+		create_info.pQueueCreateInfos=&queue_create_info;
+		create_info.queueCreateInfoCount=1;
+
+		create_info.pEnabledFeatures=&device_features;
+
+		create_info.enabledExtensionCount=0;
+
+		if(enable_validation_layers) {
+			create_info.enabledLayerCount=static_cast<uint32_t>(validation_layers.size());
+			create_info.ppEnabledLayerNames=validation_layers.data();
+		} else {
+			create_info.enabledLayerCount=0;
+		}
+
+		if(vkCreateDevice(physical_device, &create_info, nullptr, &device)!=VK_SUCCESS) {
+			throw std::runtime_error("failed to create logical device!");
+		}
+
+		vkGetDeviceQueue(device, indices.graphics_family.value(), 0, &graphics_queue);
+	}
+
 	void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
 		pickPhysicalDevice();
+		createLogicalDevice();
 	}
 
 	void initWindow() {
@@ -235,6 +275,8 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyDevice(device, nullptr);
+		
 		if(enable_validation_layers) {
 			DestroyDebugUtilsMessengerEXT(instance, debug_messenger, nullptr);
 		}
