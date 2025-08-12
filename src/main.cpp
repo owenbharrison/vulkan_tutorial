cf582846139b59156ce8cf390ac447dfde9614b5
Owen Harrison <owenbharrison@gmail.com>
2025-08-12 15:44:03 -0600

    instance

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#include <vector>
+
 const uint32_t WIDTH=800;
 const uint32_t HEIGHT=600;
 class HelloTriangleApplication {
@@ -18,7 +20,8 @@ public:
 
 private:
 	GLFWwindow* window=nullptr;
-	
+	VkInstance instance;
+
 	void initWindow() {
 		glfwInit();
 
@@ -31,8 +34,49 @@ private:
 		window=glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
-	void initVulkan() {
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType=VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName="Hello Triangle";
+		appInfo.applicationVersion=VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName="No Engine";
+		appInfo.engineVersion=VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion=VK_API_VERSION_1_0;
 
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType=VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo=&appInfo;
+
+		uint32_t glfwExtensionCount=0;
+		const char** glfwExtensions;
+
+		glfwExtensions=glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount=glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames=glfwExtensions;
+
+		createInfo.enabledLayerCount=0;
+
+		if(vkCreateInstance(&createInfo, nullptr, &instance)!=VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		uint32_t extensionCount=0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+	
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+	
+		std::cout<<"available extensions:\n";
+
+		for(const auto& extension:extensions) {
+			std::cout<<'\t'<<extension.extensionName<<'\n';
+		}
+	}
+
+	void initVulkan() {
+		createInstance();
 	}
 
 	void mainLoop() {
@@ -42,6 +86,8 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+		
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
